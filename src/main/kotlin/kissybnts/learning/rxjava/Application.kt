a7992a722d7080719f97bbdf30c2325fd80fe7db
kissybnts <fstt.fwmym.f15@gmail.com>
2017-03-09 20:48:40 +0900

    part 1 - list 18 のサンプルを追加

--- src/main/kotlin/kissybnts/learning/rxjava/Application.kt
+++ src/main/kotlin/kissybnts/learning/rxjava/Application.kt
@@ -6,7 +6,7 @@ class Application {
         fun main(args: Array<String>) {
             val part1 = Part1()
             part1.list11()
-            part1.list17()
+            part1.list18()
         }
     }
 
--- src/main/kotlin/kissybnts/learning/rxjava/Part1.kt
+++ src/main/kotlin/kissybnts/learning/rxjava/Part1.kt
@@ -1,16 +1,18 @@
 package kissybnts.learning.rxjava
 
-import io.reactivex.BackpressureStrategy
-import io.reactivex.Flowable
-import io.reactivex.FlowableOnSubscribe
+import io.reactivex.*
+import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import org.reactivestreams.Subscriber
 import org.reactivestreams.Subscription
 
 internal class Part1 {
+    /**
+     * Flowable / Subscriber のサンプル関数
+     */
     fun list11() {
         val flowable = Flowable.create(FlowableOnSubscribe<String> { e ->
-            e?: return@FlowableOnSubscribe
+            e ?: return@FlowableOnSubscribe
 
             val datas = arrayOf("Hello world", "こんにちは世界")
 
@@ -31,7 +33,7 @@ internal class Part1 {
             // the function to be used when started to subscribe
             override fun onSubscribe(s: Subscription?) {
                 // to keep the Subscription in this Subscriber
-                this.subscription = s?: throw IllegalArgumentException("subscription is null")
+                this.subscription = s ?: throw IllegalArgumentException("subscription is null")
                 // to request next data
                 this.subscription.request(1)
             }
@@ -54,5 +56,42 @@ internal class Part1 {
                 t?.printStackTrace()
             }
         })
+    }
+
+    /**
+     * Observable / Observer のサンプル関数
+     */
+    fun list18() {
+        val observable = Observable.create(ObservableOnSubscribe<String> { e ->
+            val datas = arrayOf("Hello world using Observable", "こんにちは世界、Observableを使って")
+
+            for (data in datas) {
+                if (e.isDisposed) {
+                    return@ObservableOnSubscribe
+                }
+
+                e.onNext(data)
+            }
+
+            e.onComplete()
+        })
+
+        observable.observeOn(Schedulers.computation())
+                .subscribe(object : Observer<String> {
+                    override fun onSubscribe(d: Disposable) { }
+
+                    override fun onNext(t: String) {
+                        println("${Thread.currentThread().name}: $t")
+                    }
+
+                    override fun onComplete() {
+                        println("${Thread.currentThread().name}: 完了しました")
+                    }
+
+                    override fun onError(e: Throwable?) {
+                        e?.printStackTrace()
+                    }
+                })
+        Thread.sleep(2000)
     }
 }
